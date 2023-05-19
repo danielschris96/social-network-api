d22385c9bd5110cb21ac8c054ff728cfbd903a41
Chris Daniels <danielschris96@gmail.com>
2023-05-18 21:39:44 -0700

    completed models

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Schema.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+  },
+},
+{
+  toJSON: {
+    getters: true
+  }
+});
+
+module.exports = reactionSchema;
---
+++ models/Thought.js
@@ -0,0 +1,62 @@
+const { Schema } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const moment = require('moment');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+      },
+      username: {
+        type: String,
+        required: true
+      },
+      reactions: [reactionSchema]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      }
+    });
+
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+Thought.find({}).exec((err, collection) => {
+    if (err) {
+        console.error("Error fetching thoughts:", err);
+    } else if (collection.length === 0) {
+        Thought.insertMany([
+            {
+              "thoughtText": "I love coding in JavaScript!",
+              "username": "johnDoe"
+            },
+            {
+              "thoughtText": "Mongodb is really flexible, it's great for our project.",
+              "username": "janeDoe"
+            },
+            {
+              "thoughtText": "Express.js makes server setup a breeze!",
+              "username": "jimmyDoe"
+            },
+            {
+              "thoughtText": "I'm getting the hang of full stack development!",
+              "username": "jennyDoe"
+            }
+          ], (err) => {
+            if (err) {
+                console.error("Error inserting thoughts:", err);
+            }
+        });
+    }
+});
+
+module.exports = Thought;
