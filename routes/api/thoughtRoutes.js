5d93a5bba8d8d4708ff081cbcc24621269ee8f4d
Chris Daniels <danielschris96@gmail.com>
2023-05-20 16:01:26 -0700

    finished routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,10 +6,12 @@ const {
     createThought,
     deleteThought,
     updateThought,
-    addReaction,
-    removeReaction,
+    createReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtController');
 
-
+router.route('/').get(getThoughts).post(createThought);
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
 
 module.exports = router;
